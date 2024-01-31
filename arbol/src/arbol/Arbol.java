99915a331dad2c2fc69463d4b98cc6d5939acae3
benat merino <ikctr@plaiaundi.net>
2024-01-31 13:12:15 +0100

    todos los cambios

--- arbol/src/arbol/Arbol.java
+++ arbol/src/arbol/Arbol.java
@@ -6,76 +6,136 @@ public class Arbol {
 	private String nombreComun;
 	private String nombreCientifico;
 	private String habitat;
-	private int altura;
+	private boolean singular;
 	private String origen;
+	private Habitad habitats;
 	
-	public Arbol(int id, String nombreComun, String nombreCientifico, String habitat, int altura, String origen) {
+	
+	
+	
+	public Arbol(int id, String nombreComun, String nombreCientifico, String habitat, boolean singular, String origen,
+			Habitad habitats) {
+		
 		this.id = id;
 		this.nombreComun = nombreComun;
 		this.nombreCientifico = nombreCientifico;
 		this.habitat = habitat;
-		this.altura = altura;
+		this.singular = singular;
 		this.origen = origen;
+		this.habitats = habitats;
 	}
 
-	public Arbol() {
-		
-	}
+
+
 
 	public int getId() {
 		return id;
 	}
 
+
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+
+
 
 	public String getNombreComun() {
 		return nombreComun;
 	}
 
+
+
+
 	public void setNombreComun(String nombreComun) {
 		this.nombreComun = nombreComun;
 	}
+
+
+
 
 	public String getNombreCientifico() {
 		return nombreCientifico;
 	}
 
+
+
+
 	public void setNombreCientifico(String nombreCientifico) {
 		this.nombreCientifico = nombreCientifico;
 	}
+
+
+
 
 	public String getHabitat() {
 		return habitat;
 	}
 
+
+
+
 	public void setHabitat(String habitat) {
 		this.habitat = habitat;
 	}
 
-	public int getAltura() {
-		return altura;
+
+
+
+	public boolean isSingular() {
+		return singular;
 	}
 
-	public void setAltura(int altura) {
-		this.altura = altura;
+
+
+
+	public void setSingular(boolean singular) {
+		this.singular = singular;
 	}
+
+
+
 
 	public String getOrigen() {
 		return origen;
 	}
 
+
+
+
 	public void setOrigen(String origen) {
 		this.origen = origen;
 	}
 
+
+
+
+	public Habitad getHabitats() {
+		return habitats;
+	}
+
+
+
+
+	public void setHabitats(Habitad habitats) {
+		this.habitats = habitats;
+	}
+
+
+
+
 	@Override
 	public String toString() {
-		return "albol [id=" + id + ", nombreComun=" + nombreComun + ", nombreCientifico=" + nombreCientifico
-				+ ", habitat=" + habitat + ", altura=" + altura + ", origen=" + origen + "]";
+		return "Arbol [id=" + id + ", nombreComun=" + nombreComun + ", nombreCientifico=" + nombreCientifico
+				+ ", habitat=" + habitat + ", singular=" + singular + ", origen=" + origen + ", habitats=" + habitats
+				+ "]";
 	}
+
 	
+	
+
 	
 	
 	
--- arbol/src/arbol/Habitad.java
+++ arbol/src/arbol/Habitad.java
@@ -6,4 +6,26 @@ public class Habitad {
 	private String nombre;
 	
 	
+	public Habitad() {
+
+	}
+	
+	public Habitad(int id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 }
